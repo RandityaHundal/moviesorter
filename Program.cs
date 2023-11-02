8a58ea809e9296ea8be69cf329c9bdddc3451437
RandityaHundal <randyhundal005@gmail.com>
2023-11-02 10:05:59 -0700

    test

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string[] movies = new string[3];
+
+Console.WriteLine("Input 3 movies.");
+for (int i = 0; i < movies.Length; i++)
+{
+    movies[i] = Console.ReadLine();
+}
+
+Console.WriteLine("\nHere they are alphabetically");
+
+Array.Sort(movies);
+
+for (int i = 0; i < movies.Length; i++)
+{
+    Console.WriteLine(movies[i]);
+}
+
+// close key
+Console.ReadKey();
